953c48614dc4f3a88b5cc7922e51d130aa346a33
Tim Huegdon <tim@nefariousdesigns.co.uk>
2010-10-13 22:29:37 +0100

    Using quad easing and longer transition.

--- a/j/carousel.js
+++ a/j/carousel.js
@@ -3,8 +3,8 @@ $(document).ready(function docReady(e) {
         visiblePanes: 2, // Default: 1
         panesToMove: 2, // Default: 1
         pagination: true, // Default: true
-        speed: 600, // Default: 200
-        easing: "easeOutElastic", // Default: "swing"
+        speed: 1000, // Default: 200
+        easing: "easeInOutQuad", // Default: "swing"
         loop: true, // Default: false
         autoplay: false, // Default: false
         hovercontrols: true, // Default: false
